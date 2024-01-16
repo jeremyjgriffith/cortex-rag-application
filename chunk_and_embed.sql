bdeddd781bb6c426873ee0cda5983ae2f3356155
jeremyjgriffith <jeremyjgriffith@gmail.com>
2024-01-16 16:43:45 -0600

    Update chunk_and_embed.sql

--- chunk_and_embed.sql
+++ chunk_and_embed.sql
@@ -1,4 +1,27 @@
 /*********************************************
+ * Create base table with the most recent 10-K
+ * for each company
+ *********************************************/
+create or replace table sec_reports_base as 
+SELECT
+    txt.sec_document_id,
+    companies.cik,
+    txt.variable_name as document_type,
+    companies.company_name,
+    companies.sic_code_category,
+    companies.sic_code_description,
+    companies.country,
+    txt.period_end_date,
+    txt.value
+FROM 
+    llm_training_essentials.cybersyn.sec_report_text_attributes AS txt
+    JOIN llm_training_essentials.cybersyn.sec_cik_index AS companies ON (companies.cik = txt.cik)
+WHERE 
+    txt.period_end_date >= '2020-01-01'
+    and document_type = '10-K Filing Text'
+qualify row_number() over (partition by companies.cik order by period_end_date desc) = 1;
+
+/*********************************************
  * Chunk content and get embeddings for chunks
  *********************************************/
 create or replace table content_chunks_10k as 
